d32e0cd740131d5f5a19246b3b879937afc263a2
nzrf <dylan@frozen.io>
2021-11-07 17:53:43 -0600

    package ideas

---
+++ baseinstall.ps1
@@ -0,0 +1,29 @@
+ECHO Configure chocolatey and windows
+choco feature disable -n allowGlobalConfirmation
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+
+ECHO Installing System Tools
+
+7zip
+citrix-workspace
+crystaldiskinfo
+crystaldiskmark
+f.lux
+Sudo
+wpd
+
+
+ECHO Installing Gaming Apps
+razer-synapse-3
+geforce-game-ready-driver
+steam
+
+ECHO Installing Multimedia Apps
+staxrip
+vlc
+
+ECHO Installing Development Tools
+etcher
+vscode
+notepadplusplus
