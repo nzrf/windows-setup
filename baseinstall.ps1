8119866ac7e9f08672c6884e8fd50bfc317cee6c
dylan <dylan@frozen.io>
2022-04-26 18:05:00 -0500

    updated

--- baseinstall.ps1
+++ baseinstall.ps1
@@ -3,33 +3,29 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco feature disable -n allowGlobalConfirmation
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install wsl2 --params "/Version:2 /Retry:true"
-choco install wsl-ubuntu-2004
+#choco install wsl2 --params "/Version:2 /Retry:true"
+#choco install wsl-ubuntu-2004
 
 
 ECHO Installing System Tools
 
-7zip
-citrix-workspace
-crystaldiskinfo
-crystaldiskmark
-f.lux
-Sudo
-wpd
+choco install 7zip
+choco install citrix-workspace
+choco install crystaldiskinfo
+choco install crystaldiskmark
+choco install Sudo
+choco install wpd
 
 
 ECHO Installing Gaming Apps
-razer-synapse-3
-geforce-game-ready-driver
-steam
+choco install geforce-game-ready-driver
+choco install steam
 
 ECHO Installing Multimedia Apps
-staxrip
-vlc
-spotify
+choco install staxrip
+choco install vlc
+choco install spotify
 
 ECHO Installing Development Tools
-etcher
-vscode
-notepadplusplus
-microsoft-windows-terminal
+choco install rufus
+choco install vscode
